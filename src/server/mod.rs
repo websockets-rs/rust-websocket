ae82b6f9f2ae2bd0d15e4a62d6bb675d85c82ab6
Stefan <hoitaus@gmail.com>
2017-05-19 13:16:33 +1000

    remove actual test for unsecure server

--- src/server/mod.rs
+++ src/server/mod.rs
@@ -274,7 +274,7 @@ impl Iterator for Server<NoSslAcceptor> {
 
 mod tests {
 	#[test]
-	// test the set_nonblocking() method for Server<SslAcceptor> and Server<NoSslAcceptor>. I have no idea what I'm doing here, I copied some of it from https://doc.rust-lang.org/src/std/net/tcp.rs.html#1413
+	// test the set_nonblocking() method for Server<NoSslAcceptor>. Some of this is copied from https://doc.rust-lang.org/src/std/net/tcp.rs.html#1413
 	fn set_nonblocking() {
 		macro_rules! t {
 			($e:expr) => {
@@ -285,10 +285,6 @@ mod tests {
 			}
 		}
 		use super::*;
-		use std::io::Read;
-		use openssl::ssl::{SslMethod, SslAcceptorBuilder};
-		use std::fs::File;
-		use openssl::pkcs12::Pkcs12;
 
 		// Test unsecure server
 
@@ -304,28 +300,6 @@ mod tests {
 			// nobody tried to establish a connection, so we expect an error
             Ok(_) => panic!("expected error"),
 			// do we need to check the type of error? If so, how?
-			_ => {},
-        }
-
-		// Test secure server
-		let mut file = File::open("identity.pfx").unwrap();
-		let mut pkcs12 = vec![];
-		file.read_to_end(&mut pkcs12).unwrap();
-		let pkcs12 = Pkcs12::from_der(&pkcs12).unwrap();
-		let identity = pkcs12.parse("password123").unwrap();
-
-		let acceptor = SslAcceptorBuilder::mozilla_intermediate(SslMethod::tls(),
-                                                       &identity.pkey,
-                                                       &identity.cert,
-                                                       &identity.chain)
-                  .unwrap()
-                  .build();
-		let mut server = t!(Server::bind_secure("127.0.0.1:0", acceptor));
-		t!(server.set_nonblocking(true));
-
-		let result = server.accept();
-		match result {
-            Ok(_) => panic!("expected error"),
 			_ => {},
         }
 
