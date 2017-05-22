86e922995af1f2d157801bcf6c54e92684bde469
Michael Eden <themichaeleden@gmail.com>
2017-05-21 20:08:37 -0400

    Fully async-api-ed autobahn client harness.

--- examples/autobahn-async-client.rs
+++ examples/autobahn-async-client.rs
@@ -4,11 +4,11 @@ extern crate futures;
 
 use websocket::{ClientBuilder, OwnedMessage};
 use websocket::result::WebSocketError;
-use tokio_core::reactor::{Core, Handle};
+use tokio_core::reactor::Core;
 use futures::sink::Sink;
 use futures::stream::Stream;
-use futures::{future, Future};
-use futures::future::Loop;
+use futures::Future;
+use futures::future::{self, Loop};
 
 fn main() {
 	let addr = "ws://127.0.0.1:9001".to_string();
@@ -19,15 +19,12 @@ fn main() {
 	println!("Using fuzzingserver {}", addr);
 	println!("Using agent {}", agent);
 
-	let case_count = get_case_count(addr.clone(), &handle);
+	let case_count = get_case_count(addr.clone(), &mut core);
 	println!("We will be running {} test cases!", case_count);
-
-	// let mut all_tests = Vec::with_capacity(case_count);
 
 	println!("Running test suite...");
 	for case_id in 1..(case_count + 1) {
 		let url = addr.clone() + "/runCase?case=" + &case_id.to_string()[..] + "&agent=" + agent;
-		let handle = core.handle();
 
 		let test_case = ClientBuilder::new(&url)
 			.unwrap()
@@ -37,7 +34,7 @@ fn main() {
 				future::loop_fn(duplex, |stream| {
 					stream.into_future()
 					      .or_else(|(err, stream)| {
-						               println!("message transmission was an error: {:?}", err);
+						               println!("Could not receive message: {:?}", err);
 						               stream.send(OwnedMessage::Close(None)).map(|s| (None, s))
 						              })
 					      .and_then(|(msg, stream)| match msg {
@@ -66,52 +63,47 @@ fn main() {
 					                    }
 					                    None => future::ok(Loop::Break(())).boxed(),
 					                })
-					      .map_err(|e| {
-						               println!("Error sending or other: {:?}", e);
-						               e
-						              })
 				})
 			})
+			.map(move |_| {
+				     println!("Test case {} is finished!", case_id);
+				    })
 			.or_else(move |err| {
 				         println!("Test case {} ended with an error: {:?}", case_id, err);
 				         Ok(()) as Result<(), ()>
 				        });
 
-		// all_tests.push(test_case);
 		core.run(test_case).ok();
 	}
 
-	// core.run(future::join_all(all_tests)).ok();
-
-	update_reports(addr.clone(), agent, &handle);
+	update_reports(addr.clone(), agent, &mut core);
+	println!("Test suite finished!");
 }
 
-fn get_case_count(addr: String, handle: &Handle) -> usize {
+fn get_case_count(addr: String, core: &mut Core) -> usize {
 	let url = addr + "/getCaseCount";
+	let err = "Unsupported message in /getCaseCount";
 
-	ClientBuilder::new(&url)
+	let counter = ClientBuilder::new(&url)
 		.unwrap()
-		.async_connect_insecure(&handle)
-		.and_then(|(stream, _)| stream.into_future().map_err(|e| e.0))
+		.async_connect_insecure(&core.handle())
+		.and_then(|(s, _)| s.into_future().map_err(|e| e.0))
 		.and_then(|(msg, _)| match msg {
 		              Some(OwnedMessage::Text(txt)) => Ok(txt.parse().unwrap()),
-		              _ => Err(WebSocketError::ProtocolError("Unsupported message in /getCaseCount")),
-		          })
-		.wait()
-		.unwrap()
+		              _ => Err(WebSocketError::ProtocolError(err)),
+		          });
+	core.run(counter).unwrap()
 }
 
-fn update_reports(addr: String, agent: &str, handle: &Handle) {
+fn update_reports(addr: String, agent: &str, core: &mut Core) {
 	println!("Updating reports...");
 	let url = addr + "/updateReports?agent=" + agent;
 
-	ClientBuilder::new(&url)
+	let updater = ClientBuilder::new(&url)
 		.unwrap()
-		.async_connect_insecure(&handle)
-		.and_then(|(sink, _)| sink.send(OwnedMessage::Close(None)))
-		.wait()
-		.unwrap();
+		.async_connect_insecure(&core.handle())
+		.and_then(|(sink, _)| sink.send(OwnedMessage::Close(None)));
+	core.run(updater).unwrap();
 
 	println!("Reports updated.");
-	println!("Test suite finished!");
 }
