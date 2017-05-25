a406e72b3c346cba0fe0db5d0b2e39c943fa9083
Michael Eden <themichaeleden@gmail.com>
2017-05-25 15:44:54 -0400

    Added async autobahn server, better closing behaviour.

--- examples/async-autobahn-server.rs
+++ examples/async-autobahn-server.rs
@@ -2,20 +2,18 @@ extern crate websocket;
 extern crate futures;
 extern crate tokio_core;
 
-use std::fmt::Debug;
-
-use websocket::message::{Message, OwnedMessage};
+use websocket::message::OwnedMessage;
 use websocket::server::InvalidConnection;
 use websocket::async::Server;
 
-use tokio_core::reactor::{Handle, Core};
+use tokio_core::reactor::Core;
 use futures::{Future, Sink, Stream};
 
 fn main() {
 	let mut core = Core::new().unwrap();
 	let handle = core.handle();
 	// bind to the server
-	let server = Server::bind("127.0.0.1:2794", &handle).unwrap();
+	let server = Server::bind("127.0.0.1:9002", &handle).unwrap();
 
 	// time to build the server's future
 	// this will be a struct containing everything the server is going to do
@@ -25,27 +23,16 @@ fn main() {
         // we don't wanna save the stream if it drops
         .map_err(|InvalidConnection { error, .. }| error)
         .for_each(|(upgrade, addr)| {
+            // accept the request to be a ws connection
             println!("Got a connection from: {}", addr);
-            // check if it has the protocol we want
-            if !upgrade.protocols().iter().any(|s| s == "rust-websocket") {
-                // reject it if it doesn't
-                spawn_future(upgrade.reject(), "Upgrade Rejection", &handle);
-                return Ok(());
-            }
-
-            // accept the request to be a ws connection if it does
             let f = upgrade
-                .use_protocol("rust-websocket")
                 .accept()
-                // send a greeting!
-                .and_then(|(s, _)| s.send(Message::text("Hello World!").into()))
-                // simple echo server impl
-                .and_then(|s| {
+                .and_then(|(s, _)| {
+                    // simple echo server impl
                     let (sink, stream) = s.split();
                     stream
                     .take_while(|m| Ok(!m.is_close()))
                     .filter_map(|m| {
-                        println!("Message from Client: {:?}", m);
                         match m {
                             OwnedMessage::Ping(p) => Some(OwnedMessage::Pong(p)),
                             OwnedMessage::Pong(_) => None,
@@ -58,17 +45,11 @@ fn main() {
                     })
                 });
 
-            spawn_future(f, "Client Status", &handle);
+	          handle.spawn(f.map_err(move |e| println!("{}: '{:?}'", addr, e))
+	                       .map(move |_| println!("{} closed.", addr)));
             Ok(())
         });
 
 	core.run(f).unwrap();
 }
 
-fn spawn_future<F, I, E>(f: F, desc: &'static str, handle: &Handle)
-	where F: Future<Item = I, Error = E> + 'static,
-	      E: Debug
-{
-	handle.spawn(f.map_err(move |e| println!("{}: '{:?}'", desc, e))
-	              .map(move |_| println!("{}: Finished.", desc)));
-}
